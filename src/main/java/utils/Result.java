184a0a6738bd1a1b364762f488dd063dc7f0b2f6
RomanKrohin <romankrohin126@gmail.com>
2023-11-16 23:11:11 +0300

    Database upgrade

--- src/main/java/utils/Result.java
+++ src/main/java/utils/Result.java
@@ -5,19 +5,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "roman_schema.results")
+@Table(name = "results")
 public class Result {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "results_id_seq")
+    @SequenceGenerator(name= "results_id_seq" , sequenceName = "results_id_seq", allocationSize = 1)
+    @Column(name = "id")
+    private Integer id;
     @Column(name ="x")
     private String x;
     @Column(name ="y")
@@ -30,8 +32,9 @@ public class Result {
     private String execTime;
     @Column(name ="time")
     private String time;
-    
-    public Result(String x, String y, String r, String value, String execTime, String time) {
+
+    public Result(Integer id ,String x, String y, String r, String value, String execTime, String time) {
+        this.id=id;
         this.x = x;
         this.y = y;
         this.r = r;
@@ -42,51 +45,60 @@ public class Result {
 
     public Result() {
     }
-    
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public String getX() {
         return x;
     }
-    
+
     public void setX(String x) {
         this.x = x;
     }
-    
+
     public String getY() {
         return y;
     }
-    
+
     public void setY(String y) {
         this.y = y;
     }
-    
+
     public String getR() {
         return r;
     }
-    
+
     public void setR(String r) {
         this.r = r;
     }
-    
+
     public String getValue() {
         return value;
     }
-    
+
     public void setValue(String value) {
         this.value = value;
     }
-    
+
     public String getExecTime() {
         return execTime;
     }
-    
+
     public void setExecTime(String execTime) {
         this.execTime = execTime;
     }
-    
+
     public String getTime() {
         return time;
     }
-    
+
     public void setTime(String time) {
         this.time = time;
     }
--- src/main/java/utils/ResultsBean.java
+++ src/main/java/utils/ResultsBean.java
@@ -4,13 +4,18 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Transaction;
+import utils.Result;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.persistence.Query;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.mapping.Array;
 
 import lombok.Data;
 
@@ -18,21 +23,35 @@ import lombok.Data;
 @SessionScoped
 @Data
 public class ResultsBean {
-    
+
     private Result newResult= new Result();
     private List<Result> results= new ArrayList<Result>();
+    Configuration configuration = new Configuration();
+    SessionFactory sessionFactory;
 
-    public void addNewReult(){
+    public ResultsBean(){
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+        configuration.configure();
+        configuration.addAnnotatedClass(Result.class);
+        sessionFactory = configuration.buildSessionFactory();
+        results= loadCollection();
+    }
+
+    public void addNewResult(){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
         double start = System.nanoTime();
-        newResult.setValue(String.valueOf(validate(Double.valueOf(newResult.getX()), Double.valueOf(newResult.getY()), Integer.valueOf(newResult.getR()))));
+        newResult.setValue(String.valueOf(validate(Double.valueOf(newResult.getX()), Double.valueOf(newResult.getY()), Double.valueOf(newResult.getR()))));
         double execTime = Math.round(((System.nanoTime() - start) * 0.00001) * 100.0) / 100.0;
         newResult.setExecTime(String.valueOf(execTime));
         newResult.setTime(String.valueOf(LocalDate.now()));
         results.add(newResult);
+        session.persist(newResult);
         newResult= new Result();
+        session.getTransaction().commit();
     }
 
-    private Boolean validate(Double x, Double y, Integer r){
+    private Boolean validate(Double x, Double y, Double r){
         if (x>=0 && y>=0 && x<=r/2 && y<=r){
             return true;
         }
@@ -47,5 +66,15 @@ public class ResultsBean {
 
     public Result getNewResult(){
         return this.newResult;
+    }
+
+    public ArrayList<Result> loadCollection(){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        
+        Query q = session.createQuery("from Result", Result.class);
+        ArrayList<Result> list = (ArrayList<Result>) q.getResultList();
+        session.getTransaction().commit();
+        return list;
     }
 }
